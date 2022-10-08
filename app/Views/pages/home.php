922cf22f35785d3abce3ddc7422f500020a92508
Harfel12 <85319667+harfely@users.noreply.github.com>
2022-10-08 09:30:32 +0700

    update files

---
+++ app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<!-- Use template for header and footer-->
+<?= $this->extend('layout/template'); ?>
+<!-- Content hear-->
+<?= $this->section('content'); ?>
+
+
+<!-- End section content-->
+<?= $this->endSection(); ?>
